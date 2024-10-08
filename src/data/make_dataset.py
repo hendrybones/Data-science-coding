0ebfb96b54225bf3105c6cdb8947963c8de500f3
Hendrybones <hendrymwamburi56@gmail.com>
2024-10-08 22:58:57 +0300

    data proccessing

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -78,6 +78,7 @@ for f in File:
 # --------------------------------------------------------------
 
 acc_df.info()
+gyr_df.info()
 
 pd.to_datetime(df["epoch (ms)"],unit="ms")
 df["time (01:00)"].dt.month
@@ -88,21 +89,91 @@ acc_df.index=pd.to_datetime(acc_df["epoch (ms)"],unit="ms")
 gyr_df.index=pd.to_datetime(gyr_df["epoch (ms)"],unit="ms")
 
 del acc_df["epoch (ms)"]
-del acc_df["Time (01:00)"]
+del acc_df["time (01:00)"]
 del acc_df["elapsed (s)"]
 
 del gyr_df["epoch (ms)"]
-del gyr_df["Time (01:00)"]
+del gyr_df["time (01:00)"]
 del gyr_df["elapsed (s)"]
+
+acc_df.drop(columns=['epoch (ms)'], errors='ignore')
+acc_df.drop(columns=['time (01:00)'], errors='ignore')
+acc_df.drop(columns=['elapsed (s)'], errors='ignore')
+
+gyr_df.drop(columns=['epoch (ms)'], errors='ignore')
+gyr_df.drop(columns=['time (01:00)'], errors='ignore')
+gyr_df.drop(columns=['elapsed (s)'], errors='ignore')
+
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
+File= glob("../../data/raw/MetaMotion/*.csv")
+def read_data_from_file(File):
+    cc_df=pd.DataFrame()
+    gyr_df=pd.DataFrame()
+# We set a counter for the sets
+    acc_sets=1
+    gyr_sets=1
+    
+    for f in File:
+        participants = f.split("/")[-1].split("\\")[-1].split("-")[0]
+        Label =f.split("-")[1]
+        Category=f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+    
+        df =pd.read_csv(f)
+        df["participants"]=participants
+        df["Label"]=Label
+        df["Category"]=Category
+    
+        if "Accelerometer" in f:
+            df["set"]=acc_sets
+            acc_sets +=1
+            acc_df = pd.concat([acc_df, df])
+
+        if "Gyroscope" in f:
+            df["set"]=gyr_sets
+            gyr_sets +=1
+            gyr_df = pd.concat([gyr_df, df])
 
 
+    acc_df.index=pd.to_datetime(acc_df["epoch (ms)"],unit="ms")
+    gyr_df.index=pd.to_datetime(gyr_df["epoch (ms)"],unit="ms")    
+    
+    acc_df.drop(columns=['epoch (ms)'], errors='ignore')
+    acc_df.drop(columns=['time (01:00)'], errors='ignore')
+    acc_df.drop(columns=['elapsed (s)'], errors='ignore')
+
+    gyr_df.drop(columns=['epoch (ms)'], errors='ignore')
+    gyr_df.drop(columns=['time (01:00)'], errors='ignore')
+    gyr_df.drop(columns=['elapsed (s)'], errors='ignore')
+    
+    return acc_df,gyr_df
+acc_df,gyr_df=read_data_from_file(File)
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
 
+data_merged=pd.concat([acc_df.iloc[:,:3],gyr_df],axis=1)
+
+acc_df.reset_index(drop=True, inplace=True)
+gyr_df.reset_index(drop=True, inplace=True)
+data_merged = pd.concat([acc_df.iloc[:, :3], gyr_df], axis=1)
+data_merged.dropna()
+
+data_merged.columns=[
+    "acc_x",
+    "acc_y",
+    "acc_z",
+    "gyr_x",
+    "gyr_y",
+    "gyr_z",
+    "a",
+    "b",
+    "Label",
+    "category",
+    "participant",
+    "set",
+]
 
 # --------------------------------------------------------------
 # Resample data (frequency conversion)
@@ -111,6 +182,7 @@ del gyr_df["elapsed (s)"]
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
 
+data_merged[:100].resample(rule="S").mean()
 
 # --------------------------------------------------------------
 # Export dataset
